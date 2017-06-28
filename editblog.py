0891fb3e77c02c94f32e22b71ceb6ce198b8530d
fankunpeng <2361598538@qq.com>
2017-06-28 14:48:03 +0800

    add editblog.py

---
+++ editblog.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+import argparse
+import os
+import re
+import time
+
+home = '/home/fan/.github/fankunpeng.github.io/_posts/'
+#博客的markdown文档的存放文件夹
+
+parser = argparse.ArgumentParser(description='Process jekyll blog file')
+parser.add_argument('-l', '--list', action="store_true", help='list the blog title')
+parser.add_argument('-a', '--add', action="store_true", help='add a markdown file to your _posts directory with a given title')
+parser.add_argument('title', help='the title of your markdown file that you dare to add')
+parser.add_argument('-s', '--search', action="store_true", help='search a file with a fragment of its tile')
+
+args =  parser.parse_args()
+
+if args.list:
+    home_dir = os.listdir(home)
+    tit = re.compile(r'^([0-9]*)-([0-9]*)-([0-9]*)-(.*)\.markdown')
+    for filename in home_dir:
+        u = tit.match(filename)
+        print(u.group(4))
+if args.add:
+    now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    filename = "{}-{}.markdown".format(now, args.title)
+    path = home + filename
+    with open(path, 'at') as f:
+        print('---', file=f)
+        print('layout: post', file=f)
+        print('title: {}'.format(args.title), file=f)
+        print('tags: ', file=f)
+        print('---', file=f)
+if args.search:
+    se = re.compile(r'^.*%s.*\.markdown'%(args.title))
+    post_dir = os.listdir(home)
+    for filename in post_dir:
+        if se.match(filename):
+            print(filename)
