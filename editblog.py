dbce4fae0384d6fc6bd62981dd9df3e518f97db5
fankunpeng <2361598538@qq.com>
2017-06-29 09:58:52 +0800

    add the optional parameter -e used to edit the blogfile

--- editblog.py
+++ editblog.py
@@ -6,12 +6,15 @@ import time
 
 home = '/home/fan/.github/fankunpeng.github.io/_posts/'
 #博客的markdown文档的存放文件夹
+editor = 'emacs '
+#默认编辑器，用于打开和编辑markdown文档, 注意后面留个空格 
 
 parser = argparse.ArgumentParser(description='Process jekyll blog file')
 parser.add_argument('-l', '--list', action="store_true", help='list the blog title')
 parser.add_argument('-a', '--add', action="store_true", help='add a markdown file to your _posts directory with a given title')
 parser.add_argument('title', help='the title of your markdown file that you dare to add')
 parser.add_argument('-s', '--search', action="store_true", help='search a file with a fragment of its tile')
+parser.add_argument('-e', '--edit', action="store_true", help='edit the blogfile with a given tile or a given fragment of the title')
 
 args =  parser.parse_args()
 
@@ -37,3 +40,9 @@ if args.search:
     for filename in post_dir:
         if se.match(filename):
             print(filename)
+if args.edit:
+    se = re.compile(r'^.*%s.*\.markdown'%(args.title))
+    post_dir = os.listdir(home)
+    for filename in post_dir:
+        if se.match(filename):
+            os.system(editor + home + filename + ' &')
