4737733ba9e703e3b7ae73257598f3015cb88a34
fankunpeng <2361598538@qq.com>
2017-06-29 10:21:53 +0800

    all

--- editblog.py
+++ editblog.py
@@ -15,15 +15,17 @@ parser.add_argument('-a', '--add', action="store_true", help='add a markdown fil
 parser.add_argument('title', help='the title of your markdown file that you dare to add')
 parser.add_argument('-s', '--search', action="store_true", help='search a file with a fragment of its tile')
 parser.add_argument('-e', '--edit', action="store_true", help='edit the blogfile with a given tile or a given fragment of the title')
+parser.add_argument('-p', '--publish', action="store_true", help='publish to gitpages with default comment the title')
 
 args =  parser.parse_args()
 
 if args.list:
     home_dir = os.listdir(home)
-    tit = re.compile(r'^([0-9]*)-([0-9]*)-([0-9]*)-(.*)\.markdown')
+    tit = re.compile(r'^([0-9]*)-([0-9]*)-([0-9]*)-(``.*)\.markdown')
     for filename in home_dir:
         u = tit.match(filename)
         print(u.group(4))
+
 if args.add:
     now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     filename = "{}-{}.markdown".format(now, args.title)
@@ -34,15 +36,22 @@ if args.add:
         print('title: {}'.format(args.title), file=f)
         print('tags: ', file=f)
         print('---', file=f)
+
 if args.search:
     se = re.compile(r'^.*%s.*\.markdown'%(args.title))
     post_dir = os.listdir(home)
     for filename in post_dir:
         if se.match(filename):
             print(filename)
+
 if args.edit:
     se = re.compile(r'^.*%s.*\.markdown'%(args.title))
     post_dir = os.listdir(home)
     for filename in post_dir:
         if se.match(filename):
             os.system(editor + home + filename + ' &')
+
+if args.publish:
+    os.system("cd " + home)
+    os.system("git add --all && git commit -m '" + args.title + "'")
+    os.system("git push origin master")
